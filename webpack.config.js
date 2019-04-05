f61ae944b72195818a1891f693bdd20d904f778c
Francesco Piantini <francesco.piantini@gmail.com>
2019-04-05 22:45:16 +0200

    step 4: added first webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+    ],
+};
+
