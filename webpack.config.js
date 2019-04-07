35272b2d1b7b4c936eecbeda14c70bf44ff1739b
Francesco Piantini <francesco.piantini@gmail.com>
2019-04-07 12:53:36 +0200

    step 7: added babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,19 @@ module.exports = {
             template: './src/index.html'
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
 
